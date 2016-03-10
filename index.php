03a04aaf49aa4e642da73b9c4b7abc0479c1527f
chrx <chrx0621@gmail.com>
2016-03-10 11:35:48 +0800

    可以根据角色显示问题

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <?php
-    require_once 'conn.inc';
+    require_once 'src/conn.inc';
 ?>
 <h3 align="center">请选择您的角色</h3>
 <form name="roleSelect" action="src/rolesele.php" method="post">
@@ -18,17 +18,25 @@
                     $conn = Conn::getInstence();
                     $sql = "SELECT * FROM roles";
                     $result = $conn->mysqli->query($sql);
-                    echo $result->num_rows;
-                ?>
-                <input id="radio1" type="radio" name="role" value="1" checked>
-                <label for="radio1">普通学生</label>
-                <label for="radio2"></label>
-                <input id="radio2" type="radio" name="role" value="2">毕业生
-                <label>
-                    <input type="radio" name="role" value="3">
-                </label>研究生
-            </td>
+                    //echo $result->num_rows;
+                    if($row = $result->fetch_assoc()){
 
+                        echo '<input type="radio" name="role" value="'.
+                            $row['id'].'" checked>'.$row['rolename'];
+
+                    }
+                    while($row = $result->fetch_assoc()){
+
+                        echo '<input type="radio" name="role" value="'.
+                            $row['id'].'">'.$row['rolename'];
+
+                    }
+                ?>
+
+
+            </td>
+        </tr>
+        <tr>
             <td align="center">
                 <input type="submit" value="确定">
             </td>
--- src/rolesele.php
+++ src/rolesele.php
@@ -2,9 +2,50 @@
 
 header("content-type:text/html;charset=utf-8");
 
-$role = $_POST['role'];
-echo $role;
+require_once 'conn.inc';
 
+$role = $_POST['role'];
+//echo $role;
+echo '<h2 align="center">请您回答问题</h2>';
+$conn = Conn::getInstence();
+$sql = "SELECT * FROM questionsview WHERE roleid = $role ORDER BY questionid,label";
+
+$result = $conn->mysqli->query($sql);
+$temp = 0;
+echo '<table align = "center" border="0" cellspacing="10" cellpadding="0"><tr><td>';
+if($row = $result->fetch_assoc()){
+
+    $temp = $row['questionid'];
+    echo '1. '.$row['questioncontent'];
+
+}
+else{
+    echo '<h4 align="center">尚无问题</h4>';
+    exit;
+}
+
+$result->data_seek(0);
+$count = 2;
+
+while($row = $result->fetch_assoc()){
+
+    if($row['questionid'] == $temp){
+
+        echo '<br>&nbsp;&nbsp'.$row['label'].'. ' . $row['optioncontent'];
+
+    } else {
+        $temp = $row['questionid'];
+        echo "<br>$count. ".$row['questioncontent'].
+            '<br>&nbsp;&nbsp'.$row['label'].$row['optioncontent'];
+        $count += 1;
+    }
+
+}
+
+echo '</td></tr></table>';
+
+
+/*
 $dsn = "mysql:dbname=questionnaire;host=114.215.113.71";
 
 $driver_opt = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES'utf8';");
@@ -47,5 +88,4 @@ if ($stmt->execute()) {
     }
     echo "</table>";
 
-
-}
+}*/
